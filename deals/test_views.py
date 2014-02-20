1e2a5f72dbc73222b024628177e7371a47180c98
guthriec <guthriec@stanford.edu>
2014-02-19 19:58:26 -0800

    comments on test

--- deals/test_views.py
+++ deals/test_views.py
@@ -5,7 +5,14 @@ from django.test import Client
 from django.test import TestCase
 
 class PostTestCase(TestCase):
+  """
+  Basic tests for the ability of the app to accept post requests and add
+  them to the DB as appropriate.
+  """
   def setUp(self):
+    """
+    Create our test client fixture and test objects.
+    """
     self.client = Client()
     self.happy_donuts = {'name' : "Happy Donuts",
                          'address' : "111 El Camino",
@@ -22,6 +29,11 @@ class PostTestCase(TestCase):
                   'time_end' : parser.parse("2014-02-19 09:00 UTC")}
 
   def test_vendor_post(self):
+    """
+    Tests API creation of vendor on empty DB. Posts the happy_donuts dict above
+    (key-values in the dict become key-values in the POST request), checks that
+    the object can be retrived and contains some expected fields.
+    """
     with self.assertRaises(Vendor.DoesNotExist):
       first_vendor = Vendor.objects.get(id=1)
     self.client.post('/vendors/', self.happy_donuts)
@@ -31,10 +43,14 @@ class PostTestCase(TestCase):
     
 
   def test_deal_post(self):
+  """
+  Tests API creation of deal on empty DB. Adds a vendor (through the POST
+  API as well), then adds deal1 to the database. Checks that the deal
+  can be retrieved and contains some expected fields.
+  """
     with self.assertRaises(Deal.DoesNotExist):
       first_deal = Deal.objects.get(id=1)
     self.client.post('/vendors/', self.happy_donuts)
-    first_vendor = Vendor.objects.get(id=1)
     self.client.post('/deals/', self.deal1)
     first_deal = Deal.objects.get(id=1)
     self.assertEqual(first_deal.vendor.name, "Happy Donuts")
