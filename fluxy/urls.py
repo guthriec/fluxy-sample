8d80e6a31e785d55683fdf906659bc43d74e7e08
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-02-25 19:35:07 -0800

    Removed mailchimp form and switched to normal form. Mailchimp API call made in backend. Uses mailchimp python module

--- fluxy/urls.py
+++ fluxy/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
   url(r'^$', views.index),
+  url(r'^subscribe$', views.subscribe),
   url(r'^admin/', include(admin.site.urls)),
   # Route /deals/ and /vendors/ resources
   url(r'^deals/$', 'deals.views.deal'),
--- fluxy/views.py
+++ fluxy/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import mailchimp
+
 
 def index(request):
   return render(request, 'fluxy/index.html')
+
+def subscribe(request):
+  try:
+    m = mailchimp.Mailchimp('f8dd77b845c2045f7df529b04427bd98-us3')
+    m.lists.subscribe('56437bae31', {'email':request.POST['email']},
+        double_optin=False)
+  except mailchimp.ListAlreadySubscribedError:
+    return HttpResponse("You've already subscribed. Thanks though!")
+    # return render(request, 'fluxy/index.html')
+  except mailchimp.Error, e:
+    return HttpResponse("An error occured %s: %s" % (e.__class__, e))
+  return HttpResponse("You're good")
