f36e9f4e744d3cdebd2f1dc00f6c8fcc09b5b348
guthriec <guthriec@stanford.edu>
2014-02-27 01:45:25 -0800

    Error messages added, not tested

--- deals/views.py
+++ deals/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 import json
 
-def get_deal(deal_id=None):
+def _get_deal(deal_id=None):
   """
   GET request handler for deals. If deal_id is specified, it serializes the
   corresponding Deal object to JSON and returns the result. Otherwise, response
@@ -22,7 +22,7 @@ def get_deal(deal_id=None):
   else:
     return Deal.objects.all()
 
-def post_deal(post_dict):
+def _post_deal(post_dict):
   """
   POST request handler for deals. Adds the specified deal to the DB.
   Assumes the POST request has the following keys, corresponding to field names:
@@ -43,7 +43,7 @@ def post_deal(post_dict):
   new_deal.save()
   return new_deal, new_deal.id
 
-def get_vendor(vendor_id=None):
+def _get_vendor(vendor_id=None):
   """
   Behaves identically to get_deal(), except with vendors.
   """
@@ -52,7 +52,7 @@ def get_vendor(vendor_id=None):
   else:
     return Vendor.objects.all()
 
-def post_vendor(post_dict):
+def _post_vendor(post_dict):
   """
   POST request handler for vendors. Adds the specified vendor to the DB.
   Assumes the POST request has the following keys, corresponding to field names:
@@ -72,6 +72,29 @@ def post_vendor(post_dict):
   new_vendor.save()
   return new_vendor, new_vendor.id
 
+def _make_get_response(qset, known_error = None):
+  if not known_error and qset.count() == 0:
+    known_error = {'code': 404, 'message': 'No resource found'}
+  if known_error:
+    code = known_error['code']
+    err_message = known_error['message']
+    return HttpResponse(json.dumps(known_error),\
+                        content_type="application/json", status=code)
+  else:
+    return HttpResponse(serializers.serialize("json", qset),\
+                        content_type="application/json", status=200)
+
+def _make_post_response(obj, redirect_addr, known_error = None):
+  if known_error:
+    code = known_error['code']
+    err_message = known_error['message']
+    return HttpResponse(json.dumps(known_error),\
+                        content_type="application/json", status=code)
+
+  else:
+    return HttpResponseRedirect(redirect_addr, serializers.serialize("json", [obj]),\
+                                content_type="application/json", status=201)
+
 @require_http_methods(["GET", "POST"])
 def deal(request, deal_id=None):
   """
@@ -80,29 +103,22 @@ def deal(request, deal_id=None):
   (regardless of success).
   """
   if request.method == 'GET':
+    known_error = None
+    deal_set = None
     try:
-      deal_set = get_deal(deal_id)
+      deal_set = _get_deal(deal_id)
     except Exception:
-      err = [{error: "Server error"}]
-      return HttpResponse(serializers.serialize("json", err),\
-                          content_type="application/json", status=500)
-    if deal_set.count() == 0:
-      err = [{error: "No such deal found"}]  
-      return HttpResponse(serializers.serialize("json", err),\
-                          content_type="application/json", status=404)
-    return HttpResponse(serializers.serialize("json", deal_set),\
-                        content_type="application/json", status=200)
+      known_error = {'code': 500, 'message': 'Server error'}
+    return _make_get_response(deal_set, known_error)
   else:
     # POST request.
+    known_error = None
+    deal = None
     try:
-      deal, deal_id = post_deal(request.POST)
+      deal, deal_id = _post_deal(request.POST)
     except Exception:
-      err = [{error: "Server error"}]
-      return HttpResponse(serializers.serialize("json", err),\
-                          content_type="application/json", status=500)
-    return HttpResponseRedirect('deals/' + str(deal_id),\
-                                serializers.serialize("json", [deal]),\
-                                content_type="application/json", status=201)
+      known_error = {'code': 500, 'message': 'Server error'}
+    return _make_post_response(deal, 'deals/' + str(deal_id), known_error)
 
 @require_http_methods(["GET", "POST"])
 def vendor(request, vendor_id=None):
@@ -112,16 +128,23 @@ def vendor(request, vendor_id=None):
   (regardless of success).
   """
   if request.method == 'GET':
-    vendor_set = get_vendor(vendor_id)
-    return HttpResponse(serializers.serialize("json", vendor_set),\
-                        content_type="application/json", status=200)
+    known_error = None
+    vendor_set = None
+    try:
+      vendor_set = _get_vendor(vendor_id)
+    except Exception:
+      known_error = {'code': 500, 'message': 'Server error'}
+    return _make_get_response(vendor_set, known_error)
   else:
-    # POST request. Ignore vendor_id.
-    vendor, vendor_id = post_vendor(request.POST)
-    return HttpResponseRedirect('vendors/' + str(vendor_id),\
-                                serializers.serialize("json", [vendor]),\
-                                content_type="application/json", status=201)
-
+    # POST request.
+    known_error = None
+    vendor = None
+    try:
+      vendor, vendor_id = _post_vendor(request.POST)
+    except Exception:
+      known_error = {'code': 500, 'message': 'Server error'}
+    return _make_post_response(vendor, 'vendors/' + str(vendor_id), known_error)
+    
 @require_http_methods(["GET"])
 def mock_deal(request, deal_id=None):
   deal_set = []
