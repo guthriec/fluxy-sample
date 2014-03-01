05afeabbbacc788ea742d8fd6aa4c0ee6541edbd
guthriec <guthriec@stanford.edu>
2014-02-28 16:22:47 -0800

    removed unnecessary files from merge

--- static/js/dashboard_index.js
+++
@@ -1,7 +0,0 @@
-var DashboardIndexView = Backbone.View.extend({
-  render: function() {
-    var template = _.template($("#dashIndexTemplate").html());
-    this.$el.html(template);
-    return this;
-  }
-});
--- static/js/deals_create.js
+++
@@ -1,9 +0,0 @@
-DealCreateView = Backbone.View.extend({
-  events: {
-    // on submit
-  },
-  render: function() {
-    this.$el.html(_.template($('#dealFormTemplate').html()));
-    return this;
-  }
-});
