a37d58fddda8ae21e7d285eb0ca67f18b987f6c4
guthriec <guthriec@stanford.edu>
2014-03-10 00:29:00 -0700

    took out a separate test for embedded vendor

--- deals/test_deals.py
+++ deals/test_deals.py
@@ -7,10 +7,10 @@ class DealTestCase(TestCase):
   fixtures = ['deals.json']
   
   def setUp(self):
-    """
-    Create our test client object.
-    """
+    # Create our test client object.
     self.client = Client()
+
+    # Set constants based on test fixture
     self.deal_list = list(range(1, 8))
     self.inactive_list = [3, 7]
     self.mile_pt = (37.407959, -122.121454)
@@ -20,18 +20,27 @@ class DealTestCase(TestCase):
   def test_single_deal(self):
     """
     @author: Chris
-    Tests that /deal/2 returns an object with 'title' and 'vendor/name' fields
+    Tests that /deal/2 returns an object with 'title' field
     as expected.
     """
     response = self.client.get('/api/v1/deal/2/')
     deal_obj = json.loads(response.content)[0]
     self.assertEqual(deal_obj['title'], "40% off coffee")
+  
+  def test_embedded_vendor(self):
+    """
+    @author: Chris
+    Tests that /deal/2 returns an object with 'vendor/name' field
+    as expected.
+    """
+    response = self.client.get('/api/v1/deal/2/')
+    deal_obj = json.loads(response.content)[0]
     self.assertEqual(deal_obj['vendor']['name'], "Happy Donuts")
    
   def test_deal_not_found(self):
     """
     @author: Chris
-    Tests that /deal/100 returns a 404
+    Tests that hitting a nonexistent deal (id: 100) returns a 404
     """
     response = self.client.get('/api/v1/deal/100/')
     self.assertEqual(response.status_code, 404)
@@ -39,11 +48,11 @@ class DealTestCase(TestCase):
   def test_single_deal_inactive(self):
     """
     @author: Chris
-    Tests that /deal/3 returns an object as expected
+    Tests that hitting an expired deal (/deal/3) returns an object as expected with
+    embedded vendor field.
     """
     response = self.client.get('/api/v1/deal/3/')
     deal_obj = json.loads(response.content)[0]
-    # Should still return the object
     self.assertEqual(deal_obj['title'], "20% off shitty sandwiches")
     self.assertEqual(deal_obj['vendor']['name'], "Happy Donuts")
 
