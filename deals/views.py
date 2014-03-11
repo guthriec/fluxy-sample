597930b74d3e7587ef1ef2cc774b2723bce27c07
Ayush Sood <ayushsood@gmail.com>
2014-03-10 21:47:16 -0700

    Revert 3a55bd2..dd0419f
    This rolls back to commit 3a55bd293b2e7829ae6617e178a7b3a69f9bd9f9.

--- deals/views.py
+++ deals/views.py
@@ -64,27 +64,19 @@ def vendor(request, vendor_id=None):
   if request. method == 'GET':
     known_error = None
     vendor_list = None
-
-    vendor_set = _get_vendor(vendor_id)
-    if vendor_id and vendor_set.count() == 0:
-      known_error = { 'code': 404, 'message': 'Vendor not found' }
-      return _make_get_response(vendor_list, known_error)
-
-    vendor_list = _list_from_qset(vendor_set, include_nested=False, flatten=True)
-  
-    return _make_get_response(vendor_list, known_error)
+    try:
+      vendor_set = _get_vendor(vendor_id)
+      vendor_list = _list_from_qset(vendor_set, include_nested=False)
+    except Exception:
+      known_error = { 'code': 500, 'message': 'Server error' }
+    return _make_get_response(vendor_list, known_error,\
+                              flatten=True, include_nested=False)
   else:
     # POST request.
     known_error = None
-    vendor = None 
-    vendor_id = -1
-    try:
-      vendor = Vendor(**json.loads(request.body))
-      vendor.save()
-      vendor_id = str(vendor.id)
-    except TypeError:
-      known_error = { 'code': 400, 'message': 'Bad post request' }
-    return _make_post_response(vendor, 'vendors/' + str(vendor_id), known_error)
+    vendor = Vendor(**json.loads(request.body))
+    vendor.save()
+    return _make_post_response(vendor, 'vendors/' + str(vendor.id), known_error)
 
 @require_http_methods(["GET", "POST"])
 def vendor_deals(request, vendor_id):
