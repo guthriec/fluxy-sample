4dbebb8fb2aad42cb4e748f4c0f1ee3444c31f16
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-03-13 11:01:11 -0700

    Minor bug fixes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from dashboard import views
 
 urlpatterns = patterns('',
-  url(r'^dashboard$', views.dashboard),
+  url(r'^dashboard/$', views.dashboard),
 )
