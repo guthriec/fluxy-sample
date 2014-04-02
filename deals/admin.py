05b2964e6b82be788c6b5fbe7741fb8d3925a9f5
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-04-02 11:16:05 -0700

    Registerd Users, ClaimedDeals for the admin interface

--- deals/admin.py
+++ deals/admin.py
@@ -1,5 +1,6 @@
-from deals.models import Deal, Vendor
+from deals.models import ClaimedDeal, Deal, Vendor
 from django.contrib import admin
 
+admin.site.register(ClaimedDeal)
 admin.site.register(Deal)
 admin.site.register(Vendor)
