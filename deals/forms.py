0abe43ee5118cfd042a943b7eca1dd0424911ced
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-04-07 22:15:20 -0700

    Added comments to form class

--- deals/forms.py
+++ deals/forms.py
@@ -2,5 +2,10 @@ from deals.models import Vendor
 from django.forms import ModelForm
 
 class VendorForm(ModelForm):
+  """
+  @author: Rahul
+  @desc: This is a form class based off the Vendor model used to create and
+  process forms that manipulate data for a specific Vendor object.
+  """
   class Meta:
     model = Vendor
