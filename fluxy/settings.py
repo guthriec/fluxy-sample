83ba37a8f67095efb7eaa3c292c6a960d33a0451
Ayush Sood <ayushsood@gmail.com>
2014-04-19 23:43:09 +0000

    Fixed static settings

--- fluxy/settings.py
+++ fluxy/settings.py
@@ -93,10 +93,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
 )
 
 # Login URL
@@ -110,6 +108,10 @@ AUTH_USER_MODEL = 'fluxy.FluxyUser'
 # Media files (Images, etc.)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+# Static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
 
 # Template files
 
