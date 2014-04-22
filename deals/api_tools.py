ab46880430c87ba94123e433f941613c11095529
guthriec <guthriec@stanford.edu>
2014-04-21 20:53:09 -0700

    initial make_post_response change

--- deals/api_tools.py
+++ deals/api_tools.py
@@ -27,13 +27,13 @@ def list_from_qset(qset, include_nested=False, flatten=True):
       return_list.append(obj)
   return return_list
 
-def make_post_response(obj, redirect_addr, known_error=None):
+def make_post_response(obj_list, redirect_addr, known_error=None):
   """
   @author: Ayush, Chris
   @desc: Helper function that generates appropriate response given any "known
   error" dict. Creates an appropriate response to POST request.
 
-  @param obj: object that was created
+  @param obj_list: object that was created, in JSON-serializable list form
   @param redirect_addr: address to redirect to
   @param known_error: Any known errors to include/encode in POST response
 
@@ -45,7 +45,7 @@ def make_post_response(obj, redirect_addr, known_error=None):
     return HttpResponse(json.dumps(known_error),\
                         content_type="application/json", status=code)
   else:
-    return HttpResponseRedirect(redirect_addr, serializers.serialize("json", [obj]),\
+    return HttpResponseRedirect(redirect_addr, json.dumps(obj_list),\
                                 content_type="application/json", status=201)
 
 def make_get_response(resp_list, known_error=None):
