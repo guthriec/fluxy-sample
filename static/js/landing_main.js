94097382fbfb841265eab48f56ebb0c14af94a30
guthriec <guthriec@stanford.edu>
2014-04-26 18:37:03 -0700

    took out extra stuff in landing_main

--- static/js/landing_main.js
+++ static/js/landing_main.js
@@ -1,35 +1,11 @@
 /*
  * @author: Chris
- * I didn't test this but it should be a good main function for 
+ * RequireJS Main function for landing page
  */
 
 require.config({
   paths: {
-    backbone: 'lib/backbone-1.1.2.min',
-    'backbone.wreqr': 'lib/backbone.wreqr.min',
-    bootstrap: 'lib/bootstrap.min',
     jquery: 'lib/jquery-2.1.0.min',
-    marionette: 'lib/backbone.marionette.min',
-    underscore: 'lib/underscore-1.6.0.min',
-  },
-  shim: {
-    jquery: {
-      exports: 'jQuery'
-    },
-    bootstrap: {
-      deps: ['jquery']
-    },
-    underscore: {
-      exports: '_'
-    },
-    backbone: {
-      deps: ['jquery', 'underscore'],
-      exports: 'Backbone'
-    },
-    marionette: {
-      deps: ['jquery', 'underscore', 'backbone'],
-      exports: 'Marionette'
-    },
   }
 });
 
