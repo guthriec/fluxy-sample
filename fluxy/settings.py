6d200e603f4eabced22a6c333d48dde821712362
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-05-04 19:43:37 -0700

    Included sorl-thumbnail and python-memcaching

--- fluxy/settings.py
+++ fluxy/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -72,6 +73,19 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Caching
+# https://docs.djangoproject.com/en/dev/topics/cache/
+# using python-memcached
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': [
+          '127.0.0.1:11211'
+        ]
     }
 }
 
