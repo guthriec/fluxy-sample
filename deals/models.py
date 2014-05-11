77dc23cc1dd42274486f9878543dc588b830126f
guthriec <guthriec@stanford.edu>
2014-05-11 03:17:59 -0700

    took out print statment

--- deals/models.py
+++ deals/models.py
@@ -134,7 +134,6 @@ class Deal(models.Model):
 
   def get_stage(self):
     if self.cancelled:
-      print 'cancelled!!!'
       return 4 # cancelled
     tz = self.time_start.tzinfo
     now = datetime.datetime.now(tz)
