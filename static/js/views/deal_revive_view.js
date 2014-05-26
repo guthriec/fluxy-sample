2f5db0214fe2023244ee0b18da44395c8f3da6b8
guthriec <guthriec@stanford.edu>
2014-05-26 01:59:36 -0700

    only including necessary model attributes

--- static/js/views/deal_revive_view.js
+++ static/js/views/deal_revive_view.js
@@ -23,26 +23,33 @@ define([
 
     reviveDeal: function(e) {
       e.preventDefault();
-      var newDeal = this.model.clone();
-      delete newDeal.id;
-      delete newDeal.attributes.id;
-      console.log(newDeal);
-      var oldStart = new Date(newDeal.get('time_start'));
-      var oldEnd = new Date(newDeal.get('time_end'));
+
+      var oldStart = new Date(this.model.get('time_start'));
+      var oldEnd = new Date(this.model.get('time_end'));
       var oldStartHours = oldStart.getHours(); 
       var oldStartMinutes = oldStart.getMinutes();
       var oldEndHours = oldEnd.getHours(); 
       var oldEndMinutes = oldEnd.getMinutes();
       var newStart = new Date();
       var newEnd = new Date();
+      newStart.setMilliseconds(0);
       newStart.setSeconds(0);
       newStart.setMinutes(oldStartMinutes);
       newStart.setHours(oldStartHours); 
+      newEnd.setMilliseconds(0);
       newEnd.setSeconds(0);
       newEnd.setMinutes(oldEndMinutes);
-      newEnd.setHours(oldEndHours); 
-      newDeal.set('time_start', newStart.toISOString());
-      newDeal.set('time_end', newEnd.toISOString());
+      newEnd.setHours(oldEndHours);
+      // We only copy the attributes we need
+      var newDeal = new DealModel({
+        'title': this.model.get('title'),
+        'subtitle': this.model.get('subtitle'),
+        'desc': this.model.get('desc'),
+        'max_deals': this.model.get('max_deals'),
+        'photo': this.model.get('photo'),
+        'time_start': newStart.toISOString(),
+        'time_end': newEnd.toISOString(),   
+      });
       Vent.trigger('showCreateView', newDeal);
       this.$el.find('.revive-btn').blur();
     },
