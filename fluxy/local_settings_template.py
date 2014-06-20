0a17a6374c7bc00528ca05a1ba64a213fee27569
Ayush Sood <ayushsood@gmail.com>
2014-06-20 14:20:31 -0700

    Changes for server conf

--- fluxy/local_settings_template.py
+++ fluxy/local_settings_template.py
@@ -8,3 +8,10 @@ DEBUG = TEMPLATE_DEBUG = True
 
 # People who get notified when errors happen
 ADMINS = ()
+
+# Media files (Images, etc.)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
+# Store locally for local server
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
