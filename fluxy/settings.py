b1983c6045c0a02188fae29c8fed59c34d0c940e
guthriec <guthriec@stanford.edu>
2014-06-20 16:07:20 -0700

    Revert "Changes for server conf"
    This reverts commit 0a17a6374c7bc00528ca05a1ba64a213fee27569.
    
    Conflicts:
    	fluxy/local_settings_template.py

--- fluxy/settings.py
+++ fluxy/settings.py
@@ -8,10 +8,31 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '36b2glm^*w=wz8+y&fn^s6^huvgibiaz$7++!rayba8fi)%0pd'
+
+# Facebook ID information
+FACEBOOK_APP_ID = '479319495527729';
+FACEBOOK_APP_SECRET = 'dce0bf6c3d4c0735465104def5f0b67e';
+FACEBOOK_SCOPE = ['basic_info', 'email'];
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = [
+    'fluxyapp.com',
+    'www.fluxyapp.com',
+    'http://fluxyapp.com',
+    'http://www.fluxyapp.com'
+]
 
 ADMINS = (
     ('Ayush Sood', 'ayushsood@gmail.com'),
@@ -20,71 +41,8 @@ ADMINS = (
     ('Arushi Raghuvanshi', 'arushi.raghu@gmail.com'),
     ('Amrit Saxena', 'amrit.saxena1@gmail.com'),
 )
-MANAGERS = ADMINS
 
-SERVER_EMAIL = 'Fluxy Support <support@fluxyapp.com>'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-ALLOWED_HOSTS = []
-
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
-# All media is migrated to S3 buckets
-MEDIA_ROOT = ''
-MEDIA_URL = ''
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-# Imported from local_settings
-AWS_S3_ACCESS_KEY_ID = ''
-AWS_S3_SECRET_ACCESS_KEY = ''
-AWS_STORAGE_BUCKET_NAME = ''
-
-# If one enables Access to private, the links will no longer be public
-# and the cache will break
-# AWS_DEFAULT_ACL = 'private'
-AWS_QUERYSTRING_AUTH = False
-AWS_HEADERS = {
-  'Cache-Control': 'max-age=86400', #(1 day)
-}
-
-# Static files - includes JS, CSS and Images
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-  'django.contrib.staticfiles.finders.FileSystemFinder',
-  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-  # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
-  'compressor.finders.CompressorFinder',
-)
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-  'django.template.loaders.filesystem.Loader',
-  'django.template.loaders.app_directories.Loader',
-  # 'django.template.loaders.eggs.Loader',
-)
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
-)
-
-SECRET_KEY = '36b2glm^*w=wz8+y&fn^s6^huvgibiaz$7++!rayba8fi)%0pd'
+# Application definition
 
 INSTALLED_APPS = (
     'fluxy',
@@ -117,22 +75,25 @@ AUTHENTICATION_BACKENDS = (
     'fluxy.facebook_backend.FacebookBackend',
 )
 
-# Replacing django user model
-AUTH_USER_MODEL = 'fluxy.FluxyUser'
-
-# Safer version for the signed cookie session backend
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-
-# Login URL
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
-
 ROOT_URLCONF = 'fluxy.urls'
+
 WSGI_APPLICATION = 'fluxy.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Caching
 # https://docs.djangoproject.com/en/dev/topics/cache/
 # using python-memcached
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
@@ -141,6 +102,50 @@ CACHES = {
         ]
     }
 }
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Login URL
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+
+# Replacing django user model
+
+AUTH_USER_MODEL = 'fluxy.FluxyUser'
+
+# Auth redirect
+LOGIN_URL = '/login/'
+
+# Media files (Images, etc.)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
+# Template files
+
+TEMPLATE_DIRS = [
+    os.path.join(BASE_DIR, 'templates'),
+]
 
 try:
   # Local dev settings - use local_settings_template.py as a template and move
